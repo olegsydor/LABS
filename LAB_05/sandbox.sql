a0feea0a22e99b387e8dd27b49dea9a66dd7a72d
Oleg Sydor <ole.sydor@gmail.com>
2019-07-04 10:38:18 +0300

    07-03 modified

--- LAB_05/sandbox.sql
+++ LAB_05/sandbox.sql
@@ -1,23 +1,77 @@
-/* n! */
-;WITH F_CTE (rowOrder, nFaktorial) AS
-(SELECT 1 AS rowOrder, 1 AS nFaktorial
-UNION ALL
-SELECT rowOrder+1 AS rowOrder, nFaktorial*(rowOrder+1) AS nFaktorial
-from F_CTE AS S_CTE
-where S_CTE.rowOrder < 10
-)
-SELECT TOP 1 rowOrder, nFaktorial FROM F_CTE
-ORDER BY rowOrder DESC
+﻿USE [O_SYDOR_MODULE_5]
+GO
+/*
+SELECT D.[detailid]
+      ,D.[name]
+      ,D.[color]
+      ,D.[weight]
+      ,D.[city]
+  FROM [details] AS D
 GO
 
-/* Fibonacci */
-;WITH F_CTE (rowOrder, nFibo, n0Fibo) AS
-(SELECT 1 as rowOrder, 1 as nFibo, 0 as n0Fibo
-UNION ALL
-SELECT rowOrder+1, nFibo+n0Fibo, nFibo
-from F_CTE AS S_CTE
-where S_CTE.rowOrder < 20
-)
-SELECT rowOrder, nFibo FROM F_CTE
-ORDER BY rowOrder DESC
+SELECT P.[productid]
+      ,P.[name]
+      ,P.[city]
+  FROM [products] AS P
 GO
+
+SELECT S.[supplierid]
+      ,S.[name]
+      ,S.[rating]
+      ,S.[city]
+  FROM [suppliers] AS S
+GO
+
+SELECT A.[supplierid]
+      ,A.[detailid]
+      ,A.[productid]
+      ,A.[quantity]
+  FROM [supplies] AS A
+GO
+*/
+-------------------------------------------
+
+SELECT A.*, S.*, P.*, D.*
+FROM [supplies] AS A
+JOIN [suppliers] AS S
+ON S.supplierid = A.supplierid
+JOIN [products] AS P
+ON P.productid = A.productid
+JOIN [details] AS D
+ON D.detailid = A.detailid
+
+
+---
+/* 1b.	Отримати номера і прізвища постачальників, які постачають деталі для якого-небудь виробу з деталлю 1 
+в кількості більшій, ніж середній об’єм поставок деталі 1 для цього виробу */
+
+SELECT S.supplierid, S.name, S1.* 
+FROM suppliers AS S
+JOIN supplies AS S1
+ON S1.supplierid = S.supplierid
+AND S1.detailid = 1
+--ORDER BY S1.productid
+AND S1.quantity =
+(
+SELECT AVG(S2.quantity) FROM supplies AS S2 
+GROUP BY S2.productid, S2.detailid 
+HAVING S2.detailid = S1.detailid
+AND S2.productid = S1.productid)
+
+SELECT S2.productid, AVG(S2.quantity) FROM supplies AS S2
+GROUP BY S2.productid, S2.detailid
+HAVING S2.detailid = 1
+
+
+SELECT DISTINCT SID, SNANE FROM
+(
+--SELECT S.supplierid AS SID, S.name AS N, S1.detailid AS D, S1.productid AS P, S1.quantity AS Q 
+SELECT S.supplierid AS SID, S.name AS SNANE, S1.productid AS P, S1.quantity AS Q 
+FROM suppliers AS S
+JOIN supplies AS S1
+ON S1.supplierid = S.supplierid
+AND S1.detailid = 1
+) AS X
+WHERE X.Q > (
+SELECT AVG(X1.quantity) FROM supplies AS X1 
+WHERE X1.detailid = 1 AND X1.productid = X.P)
